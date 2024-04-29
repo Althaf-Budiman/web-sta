cef4d164a2293d9737207941bb90e8ff0290abcc
Althaf-Budiman <budimanaltaf687@gmail.com>
2024-04-29 09:38:12 +0700

    check if article exists

--- resources/js/components/LatestArticlesSection.jsx
+++ resources/js/components/LatestArticlesSection.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import { ArticleItem } from "./ArticlesList";
 
 export default function LatestArticlesSection({ latestArticles }) {
+
+    let articleExists = false
+    if (latestArticles.length < 1) {
+        articleExists = false
+    } else {
+        articleExists = true
+    }
+
     return (
         <>
             <div className="mt-28 ms-20">
                 <h2 data-aos="fade-down" className="font-bold text-4xl pt-16 mb-10" id="latestArticles">Latest Articles</h2>
-                {latestArticles &&
-                    <LatestArticlesList latestArticles={latestArticles} />
+                {articleExists ?
+                    (
+                        <LatestArticlesList latestArticles={latestArticles} />)
+                    :(
+                        <p className="text-gray-400 text-center">No Article Exist.</p>
+                    )
                 }
             </div>
         </>
